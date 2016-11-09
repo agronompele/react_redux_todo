bc71cb5b506a8d91102bad3d2d24d16d6e626b58
agronom <agronom.pele@gmail.com>
2016-11-09 21:08:18 +0200

    Added webpack config code

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './client/index.js',
+    output: { path: path.join(__dirname, 'client'), filename: 'bundle.js' },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+    watch: true
+};
